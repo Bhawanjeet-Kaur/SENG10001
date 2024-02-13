c4d8def7e4173841c5bf5954736c5713496d7438
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-02-13 15:49:15 -0500

    First commit created Header comment and main function

---
+++ m2/m2/m2.cpp
@@ -0,0 +1,13 @@
+/*
+* FILE : m2.cpp
+* PROJECT : SENG1000- major assignment 2
+* PROGRAMMER : Bhawanjeet Kaur Gill
+* FIRST VERSION : 2024-02-08
+* DESCRIPTION :
+* Write a program that calculates powers using a function that you create.
+*/
+#include <stdio.h>
+int main(void)
+{
+	
+}
