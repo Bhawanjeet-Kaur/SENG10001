ae985fa6ec554ff88d37a09186e4ca880ebb6b2a
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-02-14 19:10:05 -0500

    First commit main function created

--- m2/m2/m2.cpp
+++ m2/m2/m2.cpp
@@ -9,41 +9,6 @@
 #include <stdio.h>
 int main(void)
 {
-	
-}
-/*powerCalculation() function contains two parameters in it. It is taking in a base number and an exponent for that
-* base number. The function will calculate the power of that base using while loop and an assignment operator.
-*/
-int powerCalculation(int base, int exponent)
-{
-	int calculatedPower = 1;
-	while (exponent>= 1)
-	{
-		calculatedPower *= base;
-		exponent--;
-	}
-	return calculatedPower;
-}
-/*getNum() function is a void function and is used to get an input from the user. And if the user does not enters any number, then the
-* default number will be -1.
-*/
-#pragma warning(disable: 4996)
-int getNum(void)
-{
-	/* we will see in a later lecture how we can improve this code */
-	char record[121] = { 0 }; /* record stores the string */
-	int number = 0;
-	/* NOTE to student: indent and brace this function consistent with
-	your others */
-	/* use fgets() to get a string from the keyboard */
-	fgets(record, 121, stdin);
-	/* extract the number from the string; sscanf() returns a number
-	* corresponding with the number of items it found in the string */
-	if (sscanf(record, "%d", &number) != 1)
-	{
-		/* if the user did not enter a number recognizable by
-		* the system, set number to -1 */
-		number = -1;
-	}
-	return number;
+
+
 }
